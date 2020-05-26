0bc9b27edc90f497358667b7d446c1f77ea8f360
joan-w <wamuyujoan4@gmail.com>
2020-05-26 16:45:51 +0300

    Registering the location and image category models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Galleryimage
+from .models import Galleryimage, Location, ImageCategory
 
 # Register your models here.
 admin.site.register(Galleryimage)
+admin.site.register(Location)
+admin.site.register(ImageCategory)
